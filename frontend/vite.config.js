eda5d3b0ef37cc1fa663a3bf7b7cd51e0dfa7034
Vera <veraflorentina01@gmail.com>
2025-04-24 19:21:34 +0300

    adaugari react-icons in package-json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.JPG', '**/*.jpg', '**/*.png', '**/*.svg'],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3001', // Proxy pentru backend
-    },
-  },
-});
+  build: {
+    rollupOptions: {
+      external: ['react-icons/fa']
+    }
+  }
+})
