63a1fdbcfd41835b75f3af607b7a8d40a853f4b3
Vera <veraflorentina01@gmail.com>
2025-04-24 19:30:01 +0300

    adaugare netlify.toml

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      external: ['react-icons/fa']
-    }
-  }
-})
+    outDir: './build', // sau './dist' dacă preferi
+    emptyOutDir: true, // șterge folderul la fiecare build
+  },
+});
